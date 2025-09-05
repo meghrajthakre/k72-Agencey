b4c1a06e3f652c06385cd072b288b4506a9ca35a
meghraj thakre <meghrajthkare444@gmail.com>
2025-09-05 16:19:09 +0530

    adding the agence page

--- src/components/Home/HomeBottomText.jsx
+++ src/components/Home/HomeBottomText.jsx
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom'
 
 const BottomLeft = () => {
   return (
-    <div className='font-[font1] flex items-center justify-center gap-5'>
+    <div className='font-[font1] text-white flex items-center justify-center gap-5'>
 
       <div className='border-2 hover:border-[#d3fd50]  h-24 flex items-center px-8 border-white rounded-full uppercase' >
         <Link className='text-[6vw] hover:text-[#d3fd50] mt-6' to="project">Project</Link>
       </div>
 
-      <div className='border-2 hover:border-[#d3fd50] hover:text h-24 flex items-center px-8 border-white rounded-full uppercase' >
-        <Link className='text-[6vw]  mt-6' to="agemce">Agence</Link>
+      <div className='border-2 hover:border-[#d3fd50] hover:text-[#d3fd50] h-24 flex items-center px-8 border-white rounded-full uppercase' >
+        <Link className='text-[6vw]  mt-6' to="/agence">Agence</Link>
       </div>
 
 
--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -1,8 +1,82 @@
-import React from 'react'
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
+import React, { useRef } from 'react'
 
 const Agence = () => {
+
+  const images = [
+   'https://k72.ca/uploads/teamMembers/Carl_480x640-480x640.jpg',
+    'https://k72.ca/uploads/teamMembers/Olivier_480x640-480x640.jpg',
+    'https://k72.ca/uploads/teamMembers/Lawrence_480x640-480x640.jpg',
+    'https://k72.ca/uploads/teamMembers/HugoJoseph_480x640-480x640.jpg',
+    'https://k72.ca/uploads/teamMembers/ChantalG_480x640-480x640.jpg',
+    'https://k72.ca/uploads/teamMembers/MyleneS_480x640-480x640.jpg',
+    'https://k72.ca/uploads/teamMembers/SophieA_480x640-480x640.jpg',
+    'https://k72.ca/uploads/teamMembers/Claire_480x640-480x640.jpg',
+    'https://k72.ca/uploads/teamMembers/Michele_480X640-480x640.jpg',
+    'https://k72.ca/uploads/teamMembers/MEL_480X640-480x640.jpg',
+    'https://k72.ca/uploads/teamMembers/CAMILLE_480X640_2-480x640.jpg',
+    'https://k72.ca/uploads/teamMembers/MAXIME_480X640_2-480x640.jpg',
+    'https://k72.ca/uploads/teamMembers/MEGGIE_480X640_2-480x640.jpg',
+    'https://k72.ca/uploads/teamMembers/joel_480X640_3-480x640.jpg',
+  ];
+
+
+  gsap.registerPlugin(ScrollTrigger)
+  const imageDivRef = useRef(null);
+  const imageRef = useRef(null)
+  useGSAP(function () {
+
+    gsap.to(imageDivRef.current, {
+      scrollTrigger: {
+        trigger: imageDivRef.current,
+        markers: true,
+        start: 'top 25%',
+        end: 'top -138%',
+        scrub: true,
+        pin: true,
+        onUpdate: (elem) => {
+          let imageIndex;
+          if (elem.progress < 1) {
+            imageIndex = Math.floor(elem.progress * images.length);
+
+          } else {
+            imageIndex = images.length - 1
+          }
+
+
+          imageRef.current.src = images[imageIndex]
+
+        }
+      }
+    })
+
+  })
+
   return (
-    <div>Agence</div>
+    <>
+      <div className='section-1'>
+        <div ref={imageDivRef} className='-mt-60 overflow-hidden absolute h-[20vw] w-[14vw] left-[30vw] rounded-2xl'>
+          <img ref={imageRef} className='w-full h-full object-cover' src="https://k72.ca/uploads/teamMembers/Carl_480x640-480x640.jpg" alt="" />
+        </div>
+
+        <div className='relative font-[font1]'>
+          <div className='mt-[58vh] leading-60'>
+            <h1 className='text-black text-[19vw] text-center '>SEVEN7Y
+            </h1>
+            <h1 className='text-black text-[19vw] text-center '>TWO</h1>
+          </div>
+          <div className='p-15'>
+            <p className='text-[3.5vw] -mt-20 leading-14 ml-[40%]'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;We’re inquisitive and open-minded, and we make  creativity crowds out ego from every corner. A brand is a living thing, with values, a personality and a story. If we ignore that, we can achieve short-term success, but not influence that goes the distance. We bring that perspective to every brand story we help tell.</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="section-2">
+
+      </div>
+    </>
   )
 }
 
